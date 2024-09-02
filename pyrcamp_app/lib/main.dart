e224fc0f4fa6bd352c641f208085b102aa76f61d
xkorkax <kornelia.balicka@icloud.com>
2024-09-02 11:26:40 +0200

    added folders

--- pyrcamp_app/lib/main.dart
+++ pyrcamp_app/lib/main.dart
@@ -12,7 +12,9 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       title: 'Flutter Demo',
       theme: ThemeData(
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple,),
+        colorScheme: ColorScheme.fromSeed(
+          seedColor: Colors.deepPurple,
+        ),
         useMaterial3: true,
       ),
       home: Text("JEBAC WAS"),
