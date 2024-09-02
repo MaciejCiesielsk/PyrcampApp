626d0e53a3136efb0b9f235ddeb355498ab74622
MaciejCiesielsk <maciej182.mc@gmail.com>
2024-09-02 11:13:05 +0200

    bez errora i chuj

--- pyrcamp_app/lib/main.dart
+++ pyrcamp_app/lib/main.dart
@@ -13,10 +13,10 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       title: 'Flutter Demo',
       theme: ThemeData(
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple,),
         useMaterial3: true,
       ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+      home: Text("JEBAC WAS"),
     );
   }
 }
