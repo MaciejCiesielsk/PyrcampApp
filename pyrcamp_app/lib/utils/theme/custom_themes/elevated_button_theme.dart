c2052928f8ca5626c67d77de026d331b06747db2
Maciej Ciesielski <maciej182.mc@gmail.com>
2024-09-24 10:33:43 +0200

    added country selector

--- pyrcamp_app/lib/utils/theme/custom_themes/elevated_button_theme.dart
+++ pyrcamp_app/lib/utils/theme/custom_themes/elevated_button_theme.dart
@@ -7,6 +7,9 @@ class PElevatedButtonTheme{
     style: ElevatedButton.styleFrom( // styleFrom is a simpler way to apply Button Style
       foregroundColor: const Color.fromARGB(255, 255, 255, 255),
       backgroundColor: const Color.fromARGB(255, 43, 37, 22),
+      shape: RoundedRectangleBorder(
+        borderRadius: BorderRadius.circular(10)
+      ),
       elevation: 0,
     ),
   );
