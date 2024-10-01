30b91718415720f79b86dbe4ef961bab9e705956
xkorkax <kornelia.balicka@icloud.com>
2024-10-01 22:56:26 +0200

    new shop

--- pyrcamp_app/lib/features/shop_screen/shop_screen.dart
+++ pyrcamp_app/lib/features/shop_screen/shop_screen.dart
@@ -19,12 +19,12 @@ class _ShopScreenState extends State<ShopScreen> {
   var _productName = '';
   var _productPrice = '';
   var _productImagePath = '';
-  var _currentCategory = '';
+  var _currentCategory = 'shop_food';
 
   //methods
-  StreamBuilder _viewItems() {
+  StreamBuilder _loadItems(String collectionName) {
     return StreamBuilder(
-      stream: FirebaseFirestore.instance.collection('shop_food').snapshots(),
+      stream: FirebaseFirestore.instance.collection(collectionName).snapshots(),
       builder: (ctx, snapshot) {
         if (snapshot.connectionState == ConnectionState.waiting) {
           return const Center(
@@ -37,8 +37,35 @@ class _ShopScreenState extends State<ShopScreen> {
             child: Text('No items found in this category'),
           );
         }
+        //ta czesc musi byc po ifie sprawdzajacym, czy dane istnieja, wtedy mozemy dac !
+        final loadedItems = snapshot.data!.docs;
 
-        return Text('dzialaaaaa');
+        return ListView.builder(
+          itemCount: loadedItems.length,
+          itemBuilder: (context, index) {
+            //TODO w innym pliku docelowo
+
+            final currentProduct = loadedItems[index].data();
+            _productName = currentProduct['name_pol'];
+            _productPrice = currentProduct['price'];
+            _productImagePath = currentProduct['image_path'];
+
+            return ShopTile(
+                shopItem: ShopItem(
+                    name: _productName,
+                    price: _productPrice,
+                    imagePath: _productImagePath));
+
+            /*
+            return ElevatedButton(
+              onPressed: () {},
+              child: Text(
+                currentProduct['name_pol'],
+              ),
+            );
+            */
+          },
+        );
       },
     );
   }
@@ -72,7 +99,8 @@ class _ShopScreenState extends State<ShopScreen> {
         //ta czesc musi byc po ifie sprawdzajacym, czy dane istnieja, wtedy mozemy dac !
         final loadedCategories = chatSnapshot.data!.docs;
 
-        //
+        //loading items in categories
+        //final loadedItems = _loadItems('shop_food');
 
         //normal version withot errors
         return Scaffold(
@@ -109,7 +137,7 @@ class _ShopScreenState extends State<ShopScreen> {
               Padding(
                 padding: const EdgeInsets.symmetric(horizontal: 8.0),
                 child: SizedBox(
-                  height: 50, // Fixed height for horizontal ListView
+                  height: 50,
                   child: ListView.builder(
                     itemCount: loadedCategories.length,
                     scrollDirection: Axis.horizontal,
@@ -123,10 +151,11 @@ class _ShopScreenState extends State<ShopScreen> {
                             currentCategory['category_name_pol'],
                           ),
                           onPressed: () {
-                            _currentCategory =
-                                'shop_${currentCategory['category_name_eng']}';
-
-                            // Handle category click
+                            //handle category click
+                            setState(() {
+                              _currentCategory =
+                                  'shop_${currentCategory['category_name_eng']}';
+                            });
                           },
                         ),
                       );
@@ -152,88 +181,13 @@ class _ShopScreenState extends State<ShopScreen> {
               const SizedBox(height: 10),
 
               //products in category
+
               Expanded(
                 child: Padding(
                   padding: const EdgeInsets.all(8),
-                  child: ListView.builder(
-                    itemCount: _currentCategory.length,
-                    itemBuilder: (context, index) {
-                      //TODO w innym pliku docelowo
-                      if (_currentCategory == 'shop_food') {
-                        return _viewItems();
-                      }
-                    },
-                  ),
+                  child: _loadItems(_currentCategory),
                 ),
               )
-
-              /*
-              //do przeanalizowania jeszcze
-              Expanded(
-                child: Padding(
-                  padding: const EdgeInsets.all(8),
-                  child: ListView.builder(
-                    itemCount: loadedCategories.length,
-                    scrollDirection: Axis.vertical,
-                    itemBuilder: (context, index) {
-                      final currentCategory = loadedCategories[index];
-
-                      // Pobranie danych z zagnieżdżonej kolekcji "data"
-                      return StreamBuilder(
-                        stream: FirebaseFirestore.instance
-                            .collection('shop')
-                            .doc(currentCategory.id) // id kategorii
-                            .collection(
-                                'data') // tutaj odwołujemy się do podkolekcji
-                            .snapshots(),
-                        builder: (ctx, dataSnapshot) {
-                          if (dataSnapshot.connectionState ==
-                              ConnectionState.waiting) {
-                            return const Center(
-                              child: CircularProgressIndicator(),
-                            );
-                          }
-
-                          if (!dataSnapshot.hasData ||
-                              dataSnapshot.data!.docs.isEmpty) {
-                            return const Center(
-                              child: Text('No items found in this category'),
-                            );
-                          }
-
-                          // Tutaj pobieramy produkty z podkolekcji
-                          final loadedProducts = dataSnapshot.data!.docs;
-
-                          // Wyświetlenie przycisków dla produktów
-                          return Column(
-                            crossAxisAlignment: CrossAxisAlignment.start,
-                            children: loadedProducts.map((productDoc) {
-                              final currentProduct = productDoc.data();
-
-                              return Padding(
-                                padding:
-                                    const EdgeInsets.symmetric(vertical: 4.0),
-                                child: ElevatedButton(
-                                  child: Text(
-                                    currentProduct[
-                                        'name_pol'], // wyświetlamy nazwę produktu
-                                  ),
-                                  onPressed: () {
-                                    // Możesz tu dodać logikę, co się stanie po kliknięciu
-                                    print(
-                                        "Clicked on ${currentProduct['name_pol']}");
-                                  },
-                                ),
-                              );
-                            }).toList(),
-                          );
-                        },
-                      );
-                    },
-                  ),
-                ),
-              ),
-              */
             ],
           ),
         );
@@ -241,93 +195,3 @@ class _ShopScreenState extends State<ShopScreen> {
     );
   }
 }
-
-/*
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      backgroundColor: const Color.fromARGB(255, 66, 39, 83),
-      appBar: AppBar(
-        backgroundColor: Colors.transparent,
-        elevation: 0,
-        title: const Text('Pyrcamp Shop'),
-      ),
-      body: Column(
-        crossAxisAlignment: CrossAxisAlignment.start,
-        children: [
-          //search bar
-          Padding(
-            padding: const EdgeInsets.symmetric(horizontal: 25.0),
-            child: TextField(
-              decoration: InputDecoration(
-                focusedBorder: OutlineInputBorder(
-                  borderSide: const BorderSide(color: Colors.white),
-                  borderRadius: BorderRadius.circular(20),
-                ),
-                enabledBorder: OutlineInputBorder(
-                  borderSide: const BorderSide(color: Colors.white),
-                  borderRadius: BorderRadius.circular(20),
-                ),
-                hintText: 'Search',
-              ),
-            ),
-          ),
-
-          const SizedBox(height: 25),
-
-          //categories
-          Padding(
-            padding: const EdgeInsets.symmetric(horizontal: 8.0),
-            child: SizedBox(
-              height: 50, // Fixed height for horizontal ListView
-              child: ListView.builder(
-                itemCount: availableCategories.length,
-                scrollDirection: Axis.horizontal,
-                itemBuilder: (context, index) => Padding(
-                  padding: const EdgeInsets.symmetric(horizontal: 4.0),
-                  child: ElevatedButton(
-                    child: Text(availableCategories[index].title),
-                    onPressed: () {
-                      // Handle category click
-                    },
-                  ),
-                ),
-              ),
-            ),
-          ),
-
-          const SizedBox(height: 25),
-
-          //menu list
-          Padding(
-            padding: const EdgeInsets.symmetric(horizontal: 25),
-            child: Text(
-              'Food Menu',
-              style: TextStyle(
-                  fontWeight: FontWeight.bold,
-                  color: Colors.grey,
-                  fontSize: 18),
-            ),
-          ),
-
-          const SizedBox(height: 10),
-
-          //popular food
-          Expanded(
-            child: Padding(
-              padding: const EdgeInsets.all(8),
-              child: ListView.builder(
-                itemCount: availableShopItems.length,
-                scrollDirection: Axis.vertical,
-                itemBuilder: (context, index) => ShopTile(
-                  shopItem: availableShopItems[index],
-                ),
-              ),
-            ),
-          ),
-        ],
-      ),
-    );
-  }
-  */
-
