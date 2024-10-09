4f998e3c1fea0077b75ee14ec6e24aa582e261a2
Kacper <kacper@debian.home>
2024-10-09 09:01:49 +0200

    deleted unused imports

--- pyrcamp_app/lib/features/form_screen/form_screen.dart
+++ pyrcamp_app/lib/features/form_screen/form_screen.dart
@@ -1,8 +1,6 @@
 import 'package:country_picker/country_picker.dart';
 import 'package:flutter/material.dart';
-import 'package:firebase_core/firebase_core.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
-import 'package:firebase_auth/firebase_auth.dart';
 
 class FormScreen extends StatefulWidget {
   const FormScreen({super.key});
