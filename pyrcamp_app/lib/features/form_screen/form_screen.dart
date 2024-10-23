8ee5c4322053a700de40d3d16c3f2cbd4c58604a
xkorkax <kornelia.balicka@icloud.com>
2024-10-23 18:47:46 +0200

    first changes in formscreen

--- pyrcamp_app/lib/features/form_screen/form_screen.dart
+++ pyrcamp_app/lib/features/form_screen/form_screen.dart
@@ -1,9 +1,10 @@
 import 'package:flutter/material.dart';
-import 'package:pyrcamp_app/features/form_screen/additional_person_form.dart';
-import 'package:pyrcamp_app/features/form_screen/main_person_form.dart';
+//import 'package:pyrcamp_app/features/form_screen/additional_person_form.dart';
+//import 'package:pyrcamp_app/features/form_screen/main_person_form.dart';
 import 'package:pyrcamp_app/features/form_screen/person.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:pyrcamp_app/main.dart';
+import 'package:country_picker/country_picker.dart';
 
 class FormScreen extends StatefulWidget {
   const FormScreen({super.key});
@@ -16,6 +17,7 @@ class _FormScreenState extends State<FormScreen> {
   final _formKey = GlobalKey<FormState>();
   var _extraPerson = false;
   var _additionalPerson = 0;
+  var _invoiceWanted = false;
   late Person mainPerson = Person();
   late Person additonalPerson1;
   late Person additonalPerson2;
@@ -31,10 +33,7 @@ class _FormScreenState extends State<FormScreen> {
     }
     _formKey.currentState!.save();
     try {
-      await FirebaseFirestore.instance
-          .collection('test_form_main')
-          .doc()
-          .set({
+      await FirebaseFirestore.instance.collection('test_form_main').doc().set({
         'name': mainPerson.name,
         'surname': mainPerson.surname,
         'address': mainPerson.address,
@@ -43,7 +42,7 @@ class _FormScreenState extends State<FormScreen> {
         'country': mainPerson.country,
         'gender': mainPerson.gender,
         'birthdate': mainPerson.birthDate,
-        'buyer': currentUser.uid
+        //'buyer': currentUser.uid
       });
     } on FirebaseException catch (error) {
       ScaffoldMessenger.of(context).clearSnackBars();
@@ -52,7 +51,6 @@ class _FormScreenState extends State<FormScreen> {
       ));
     }
   }
-
 
   @override
   Widget build(BuildContext context) {
@@ -68,7 +66,258 @@ class _FormScreenState extends State<FormScreen> {
             child: Column(
               mainAxisSize: MainAxisSize.max,
               children: <Widget>[
-                MainPersonForm(mainPerson: mainPerson,),
+                //formularz osoby kupujacej
+                Form(
+                  key: _formKey,
+                  child: Column(
+                    children: [
+                      const SizedBox(
+                        height: 20,
+                      ),
+
+                      const Text(
+                        'Osoba kupująca',
+                        style: TextStyle(
+                          fontSize: 24,
+                        ),
+                      ),
+
+                      const SizedBox(
+                        height: 20,
+                      ),
+                      // NAME
+                      TextFormField(
+                          decoration: const InputDecoration(
+                            labelText: 'Imie',
+                            border: OutlineInputBorder(),
+                          ),
+                          validator: (value) {
+                            if (value == null || value.trim().isEmpty) {
+                              return 'Please enter a valid name';
+                            }
+                            return null;
+                          },
+                          onSaved: (enteredValue) {
+                            mainPerson.name = enteredValue!;
+                          }),
+                      const SizedBox(
+                        height: 12,
+                      ),
+
+                      // SURNAME
+                      TextFormField(
+                          decoration: const InputDecoration(
+                              labelText: 'Nazwisko',
+                              border: OutlineInputBorder()),
+                          validator: (value) {
+                            if (value == null || value.trim().isEmpty) {
+                              return 'Please enter a valid surname';
+                            }
+                            return null;
+                          },
+                          onSaved: (enteredValue) {
+                            mainPerson.surname = enteredValue!;
+                          }),
+                      const SizedBox(
+                        height: 12,
+                      ),
+
+                      // ADDRESS
+                      TextFormField(
+                          decoration: const InputDecoration(
+                              labelText: 'Adres', border: OutlineInputBorder()),
+                          validator: (value) {
+                            if (value == null || value.trim().isEmpty) {
+                              return 'Please enter a valid address';
+                            }
+                            return null;
+                          },
+                          onSaved: (enteredValue) {
+                            mainPerson.address = enteredValue!;
+                          }),
+                      const SizedBox(
+                        height: 12,
+                      ),
+
+                      Row(
+                        children: [
+                          // ZIPCODE
+                          Expanded(
+                            child: TextFormField(
+                                decoration: const InputDecoration(
+                                    labelText: 'Kod pocztowy',
+                                    border: OutlineInputBorder()),
+                                validator: (value) {
+                                  if (value == null || value.trim().isEmpty) {
+                                    return 'Please enter a valid zip code';
+                                  }
+                                  return null;
+                                },
+                                onSaved: (enteredValue) {
+                                  mainPerson.zipCode = enteredValue!;
+                                }),
+                          ),
+                          const SizedBox(
+                            height: 12,
+                          ),
+                          const SizedBox(
+                            width: 5,
+                          ),
+                          // CITY
+                          Expanded(
+                            child: TextFormField(
+                              decoration: const InputDecoration(
+                                  labelText: 'Miasto',
+                                  border: OutlineInputBorder()),
+                              validator: (value) {
+                                if (value == null || value.trim().isEmpty) {
+                                  return 'Please enter a valid City';
+                                }
+                                return null;
+                              },
+                              onSaved: (enteredValue) {
+                                mainPerson.city = enteredValue!;
+                              },
+                            ),
+                          ),
+                        ],
+                      ),
+
+                      const SizedBox(
+                        height: 12,
+                      ),
+
+                      // COUNTRY
+                      Row(
+                        children: [
+                          Expanded(
+                            child: Text(
+                              mainPerson.country.isEmpty
+                                  ? 'Brak wybranego kraju'
+                                  : mainPerson.country,
+                              style: const TextStyle(fontSize: 16),
+                            ),
+                          ),
+                          const SizedBox(
+                            width: 12,
+                          ),
+                          Expanded(
+                            child: ElevatedButton(
+                              onPressed: () {
+                                showCountryPicker(
+                                  context: context,
+                                  onSelect: (Country country) {
+                                    setState(() {
+                                      mainPerson.country = country.displayName;
+                                    });
+                                  },
+                                );
+                              },
+                              child: const Text('Wybierz państwo'),
+                            ),
+                          ),
+                        ],
+                      ),
+                      const SizedBox(
+                        height: 12,
+                      ),
+
+                      // SEX
+                      DropdownButtonFormField(
+                        decoration: const InputDecoration(
+                            border: OutlineInputBorder(), label: Text('Płeć')),
+                        items: const [
+                          DropdownMenuItem(
+                            //value passed to firebase
+                            value: 'kobieta',
+                            //visible text
+                            child: Text('Kobieta'),
+                          ),
+                          DropdownMenuItem(
+                            value: 'mezczyzna',
+                            child: Text('Mężczyzna'),
+                          ),
+                          DropdownMenuItem(
+                            value: 'helikopter bojowy',
+                            child: Text('helikopter bojowy'),
+                          ),
+                        ],
+                        //onChanged is required by function constructor
+                        onChanged: (v) {},
+                        onSaved: (enteredValue) {
+                          mainPerson.gender = enteredValue!;
+                        },
+                      ),
+
+                      //const DropdownSexMenu(),
+                      const SizedBox(
+                        height: 12,
+                      ),
+
+                      // BIRTH DATE
+                      TextFormField(
+                          decoration: const InputDecoration(
+                              labelText: 'Data urodzenia',
+                              border: OutlineInputBorder()),
+                          validator: (value) {
+                            if (value == null || value.trim().isEmpty) {
+                              return 'Please enter a valid BirthDate';
+                            }
+                            return null;
+                          },
+                          onSaved: (enteredValue) {
+                            mainPerson.birthDate = enteredValue!;
+                          }),
+
+                      Row(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        children: [
+                          Checkbox(
+                            value: _invoiceWanted,
+                            onChanged: (bool? newValue) {
+                              setState(() {
+                                _invoiceWanted = newValue!;
+                              });
+                            },
+                          ),
+                          const Text('Chcę fakturę'),
+                        ],
+                      ),
+
+                      if (_invoiceWanted)
+                        Column(
+                          children: [
+                            TextFormField(
+                              decoration: const InputDecoration(
+                                  // NAME OF THE COMPANY
+                                  labelText: 'Nazwa',
+                                  border: OutlineInputBorder()),
+                            ),
+                            const SizedBox(
+                              height: 12,
+                            ),
+
+                            // NIP
+                            TextFormField(
+                              decoration: const InputDecoration(
+                                  labelText: 'NIP',
+                                  border: OutlineInputBorder()),
+                            ),
+                            const SizedBox(
+                              height: 12,
+                            ),
+
+                            // CONTACT EMAIL
+                            TextFormField(
+                              decoration: const InputDecoration(
+                                  labelText: 'E-mail kontaktowy',
+                                  border: OutlineInputBorder()),
+                            ),
+                          ],
+                        ),
+                    ],
+                  ),
+                ),
                 Row(
                   mainAxisAlignment: MainAxisAlignment.center,
                   children: [
@@ -138,9 +387,9 @@ class _FormScreenState extends State<FormScreen> {
                             const SizedBox(
                               height: 20,
                             ),
-                            AdditionalPersonForm(
-                              additionalPerson: additonalPerson1,
-                            ),
+                            //AdditionalPersonForm(
+                            //additionalPerson: additonalPerson1,
+                            //),
                           ],
                         )
                     ],
@@ -149,7 +398,9 @@ class _FormScreenState extends State<FormScreen> {
                   height: 20,
                 ),
                 ElevatedButton(
-                  onPressed: () {submit();},
+                  onPressed: () {
+                    submit();
+                  },
                   child: const Text('Prześlij formularz zgłoszeniowy'),
                 ),
                 const SizedBox(
